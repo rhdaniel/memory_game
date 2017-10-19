848776ba353361b5024e24e47893e5842843047d
Rami Daniel <Rami@Ramis-MacBook-Pro.local>
2017-10-19 15:01:02 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
 console.log("Up and running!");
-var cards ["king", "king", "queen", "queen"];
-var cardsInPlay [];
+var cards = ["king", "king", "queen", "queen"];
+var cardsInPlay = [];
 var cardOne = cards [0];
 cardsInPlay.push (cardOne);
 console.log ("User Flipped queen");
+var cardTwo;
+var cardTwo = cards [2];
+cardsInPlay.push (cardTwo);
+console.log ("User flipped king");
+if (cardsInPlay.length ===2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You Found A Match!");
+	} else {
+		alert("Sorry Try Again");
+	}
+
+}
+
 
