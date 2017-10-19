c22dd4867993563e1a6717042ff89169785405cb
Rami Daniel <Rami@Ramis-MacBook-Pro.local>
2017-10-19 12:40:18 -0400

    commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+console.log("Up and running!");
+var cards ["king", "king", "queen", "queen"];
+var cardsInPlay [];
+var cardOne = cards [0];
+cardsInPlay.push (cardOne);
+console.log ("User Flipped queen");
+
