ec089519f8b32d1206606b18a43af26aae2ccec6
Rami Daniel <Rami@Ramis-MacBook-Pro.local>
2017-10-19 18:45:58 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,6 +1,30 @@
 
 
-var cards = ["king", "king", "queen", "queen"];
+var cards = [
+{
+	rank: 'queen',
+	suit: 'hearts',
+	cardImage: "images/queen-of-hearts.png",
+
+},
+{
+	rank: 'queen',
+	suit: 'diamonds',
+	cardImage: "images/queen-of-diamonds.png",
+},
+{
+	rank: 'king',
+	suit: 'hearts',
+	cardImage: "images/king-of-hearts.png",
+
+},
+{
+	rank: 'king',
+	suit: 'diamonds',
+	cardImage: "images/king-of-diamonds.png",	
+}
+];
+
 var cardsInPlay = [];
 
 var checkForMatch = function () {
@@ -16,8 +40,10 @@ var checkForMatch = function () {
 
 var flipCard = function (cardId) {
 	checkForMatch ()
-	console.log ("User Flipped " + cards[cardId]);
-	cardsInPlay.push (cards[cardId]);
+	console.log ("User Flipped " + cards[cardId].rank);
+	cardsInPlay.push (cards[cardId].rank);
+	console.log (cards[cardId].suit);
+	console.log (cards[cardId].cardImage);
 
 }
 
